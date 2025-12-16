920b62be3eb73fc2fb1df4d6982163ec02e5b084
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2025-12-16 22:14:15 +0100

    [UPDATE]: Added dynamic menu with a submenus

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -10,42 +10,81 @@
 
     $locations = get_nav_menu_locations();
     $desktopMenuID = $locations['header_menu'];
-    $desktopMenu = wp_get_nav_menu_items($desktopMenuID);
+    $desktopMenu = buildMenuStructure(wp_get_nav_menu_items($desktopMenuID));
+
+    function buildMenuStructure($menuItems, $parentID = 0) {
+        $buffer = [];
+
+        foreach($menuItems as $item) {
+            if((int)$item->menu_item_parent === $parentID) {
+                $children = buildMenuStructure($menuItems, (int)$item->ID);
+
+                $buffer[] = [
+                    'item' => $item,
+                    'children' => $children
+                ];
+            }
+        }
+
+        return $buffer;
+    }
+
+    function generateMenus($menu, $class = 'menu', $dept = 0) {
+        echo "<ul class='". ($dept > 0 ? "{$class}" : "{$class}__list") ."'>";
+        foreach($menu as $element) {
+            echo "<li class='{$class}__item". ($element['children'] ? " {$class}__item--submenu submenu-{$dept}" : "") ."'>";
+                if($element['item']->url !== '#') {
+                    echo "<a href='{$element['item']->url}' class='{$class}__link'>";
+                        echo $element['item']->title;
+                    echo '</a>';
+                }
+                else {
+                    echo "<div class='{$class}__link'>";
+                        echo $element['item']->title;
+                    echo '</div>';
+                }
+                
+                if($element['children']) {
+                    generateMenus($element['children'], 'submenu', $dept + 1);
+                }
+            echo '</li>';
+        }
+        echo '</ul>';
+    }
 ?>
 
 <header class="header">
     <div class="header__container container">
-        <?php if(is_front_page() || is_home()) : ?>
-            <div class="header__logo">
-                <?php if($logo_file && $logo_ext === 'svg') :?>
-                    <?= $logo_svg;?>
-                <?php elseif($logo_file && $logo_ext !== 'svg') : ?>
-                    <img src="<?= esc_url($logo[0]); ?>" width="<?= $logo[1]?>" height="<?= $logo[2]?>" alt="<?= bloginfo('name'); ?>_logo">
-                <?php endif; ?>
-                <?= bloginfo('name'); ?>
+        <div class="header__body">
+            <?php if(is_front_page() || is_home()) : ?>
+                <div class="header__logo">
+                    <?php if($logo_file && $logo_ext === 'svg') :?>
+                        <?= $logo_svg;?>
+                    <?php elseif($logo_file && $logo_ext !== 'svg') : ?>
+                        <img src="<?= esc_url($logo[0]); ?>" width="<?= $logo[1]?>" height="<?= $logo[2]?>" alt="<?= bloginfo('name'); ?>_logo">
+                    <?php endif; ?>
+                    <?= bloginfo('name'); ?>
+                </div>
+            <?php else : ?>
+                <a href="<?= esc_url(home_url('/'));?>" class="header__logo">
+                    <?php if($logo_file && $logo_ext === 'svg') :?>
+                        <?= $logo_svg;?>
+                    <?php elseif($logo_file && $logo_ext !== 'svg') : ?>
+                        <img src="<?= esc_url($logo[0]); ?>" width="<?= $logo[1]?>" height="<?= $logo[2]?>" alt="<?= bloginfo('name'); ?>_logo">
+                    <?php endif; ?>
+                    <?= bloginfo('name'); ?>
+                </a>
+            <?php endif; ?>
+            <div class="header__menu menu">
+                <nav class="menu__body">
+                    <ul>
+                        <?php generateMenus($desktopMenu)?>
+                    </ul>
+                </nav>
             </div>
-        <?php else : ?>
-            <a href="<?= esc_url(home_url('/'));?>" class="header__logo">
-                <?php if($logo_file && $logo_ext === 'svg') :?>
-                    <?= $logo_svg;?>
-                <?php elseif($logo_file && $logo_ext !== 'svg') : ?>
-                    <img src="<?= esc_url($logo[0]); ?>" width="<?= $logo[1]?>" height="<?= $logo[2]?>" alt="<?= bloginfo('name'); ?>_logo">
-                <?php endif; ?>
-                <?= bloginfo('name'); ?>
-            </a>
-        <?php endif; ?>
-        <div class="header__menu menu">
-            <nav class="menu__body">
-                <ul>
-                    <?php foreach($desktopMenu as $menuItem) : ?>
-                       <li>
-                         <a href="<?=$menuItem->url?>">
-                            <?=$menuItem->title?>
-                         </a>
-                       </li>
-                    <?php endforeach; ?>
-                </ul>
-            </nav>
+            <button class="header__burger burger">
+              <span></span>
+            </button>
         </div>
     </div>
 </header>
