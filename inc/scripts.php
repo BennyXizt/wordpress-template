9f6f7aba4f206ca9a495840b44749ab3967eab2e
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2025-12-17 03:19:53 +0100

    [UPDATE]: Updated scripts.php

--- inc/scripts.php
+++ inc/scripts.php
@@ -3,9 +3,16 @@
 
 function mytheme_enqueue_assets_register() {
     wp_enqueue_style('theme-metadata', get_stylesheet_uri());
-    wp_enqueue_style('custom-main-style', get_template_directory_uri() . '', [], wp_get_theme()->get('Version'));
+    wp_enqueue_style('custom-main-style', get_template_directory_uri() . '/assets/styles/index.css', [], wp_get_theme()->get('Version'));
 
-    wp_enqueue_script('custom-main-js', get_template_directory_uri() . '', [], wp_get_theme()->get('Version'), true);
+    wp_enqueue_script('custom-main-js', get_template_directory_uri() . '/assets/js/index.js', [], wp_get_theme()->get('Version'), true);
+
+    add_filter('script_loader_tag', function ($tag, $handle) {
+        if ($handle === 'custom-main-js') {
+            $tag = str_replace('<script ', '<script type="module" ', $tag);
+        }
+        return $tag;
+    }, 10, 2);
 }
 
 
