c65fb357e919f1319119e63e0eb174a0538447a5
BennyXizt <zymayvetaliy@gmail.com>
2026-01-13 16:25:50 +0100

    [UPDATE]: Added Custom Post tYpe

--- functions.php
+++ functions.php
@@ -4,9 +4,11 @@ require_once get_template_directory() . '/inc/globals.php';
 require_once get_template_directory() . '/inc/scripts.php';
 require_once get_template_directory() . '/inc/class-custom-customize.php';
 require_once get_template_directory() . '/inc/class-custom-gutenberg.php';
+require_once get_template_directory() . '/inc/class-custom-post-types.php';
 require_once get_template_directory() . '/inc/menus.php';
 
 add_action('after_setup_theme', function() {
     new Custom_Customize();
     new Custom_Gutenberg();
+    new Custom_Post_Type();
 });
